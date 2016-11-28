5e128fbc1f3ed6c17f2a1b686459fb31529cdcf5
Jérôme Buisine <jerome.buisine@outlook.fr>
2016-11-28 21:57:41 +0100

    Project scala update

---
+++ src/com/projet/ro/main/Main.scala
@@ -0,0 +1,23 @@
+package com.projet.ro.main
+
+import com.projet.ro.utilities._
+
+object Main{
+  
+  def photoDist = Array[Array[Double]]()
+  def albumInvDist =  Array[Array[Double]]()
+  
+  val df = new java.text.DecimalFormat("0.##");
+
+  
+  def main(args: Array[String]): Unit = {
+    val file = new FileClass("example.txt")
+    //photoDist = Array.ofDim[Double](2,3)
+    file.writeLine(getValue, false)
+    println("Hello World")
+  }
+  
+  def getValue(): String = {
+    return "My value"
+  }
+}
