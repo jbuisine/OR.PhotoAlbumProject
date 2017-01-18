3024d395755d71213c2851440a2a73375a6dcc99
Jérôme Buisine <jerome.buisine@outlook.fr>
2017-01-18 22:18:18 +0100

    PLS with colors and uncommonTags score added

--- generate_Pareto_Front.R
+++ generate_Pareto_Front.R
@@ -8,13 +8,13 @@
 #Chemin absolu du repository git
 setwd("/home/jbuisine/git/M1.I2L.TD.RO.2016-11-09/scores")
 
-# lit les données du pareto local search entre la moyenne de gris et les couleurs
-df.rs <- read.table("PLS_greyAVGAndColors.xls", sep = ",")
+# lit les données du pareto local search entre la moyenne des couleurs et des tags non communs
+df.rs <- read.table("PLS_colorsAndUncommonTags.xls", sep = ",")
 
-names(df.rs) <- c("greyAVG", "colors")
+names(df.rs) <- c("colors", "uncommonTags")
 
 # vérification du fichier: affiche les premières lignes
 head(df.rs)
 
 # nuage de points obtenus en fonction des deux scores des fonctions (solution)
-plot(greyAVG ~ colors, data = df.rs, col="blue")
+plot(colors ~ uncommonTags, data = df.rs, col="blue")
