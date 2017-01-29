fd8a6b01b7d05900d2fc855243a8a7e8ebf0d183
Jérôme Buisine <jerome.buisine@outlook.fr>
2017-01-29 22:03:50 +0100

    R file refactored

--- generate_Pareto_Front.R
+++ generate_Pareto_Front.R
@@ -10,27 +10,34 @@
 # - le fichier de données en paramètres                           #
 ###################################################################
 
-#Chemin absolu du repository git
-setwd("/home/jbuisine/git/M1.I2L.TD.RO.2016-11-09/scores")
 
-# lit les données du pareto local search entre la moyenne des couleurs et des tags non communs
-df.rs <- read.table("MOEA_D_test.xls", sep = ",")
+main <- function(){
+   setwd("/home/jbuisine/git/M1.I2L.TD.RO.2016-11-09/scores")
+   
+   twoDimPlot("PLS_greyAVGAndColors.xls", ",", c("greyAVG", "colors"))
+   #threeDimPlot("PLS_greyAVGAndColorsAndCommonTags.xls", ",", c("greyAVG", "colors", "commonTags"))
+   
+}
+twoDimPlot <- function(filename, separator,  namesCoords) {
+   
+   #Read values and initialize head
+   df.rs <- read.table(filename, sep = separator)
+   names(df.rs) <- c(namesCoords[1], namesCoords[2])
+   head(df.rs)
+   print(df.rs[,1])
+   
+   #Create plot 
+   plot(df.rs[,1] ~ df.rs[,2], data = df.rs, col="blue")
+}
 
-names(df.rs) <- c("colors", "uncommonTags", "CommonTags")
-
-# vérification du fichier: affiche les premières lignes
-head(df.rs)
-
-# nuage de points obtenus en fonction des deux scores des fonctions (solution)
-plot(colors ~ uncommonTags, data = df.rs, col="blue")
-
-## Load scatterplot3d
-#library(scatterplot3d)
-
-## Simple scatter plot
-#with(data = df.rs,
-#     scatterplot3d(x = colors,
-#                   y = greyAVG,
-#                   z = CommonTags,
-#     )
-#)
+threeDimPlot <- function(filename, separator, namesCoords) {
+   df.rs <- read.table(filename, sep = separator)
+   names(df.rs) <- c(namesCoords[1], namesCoords[2], namesCoords[3])
+   head(df.rs)
+   
+   ## Load scatterplot3d
+   library(scatterplot3d)
+   
+   ## Simple scatter plot
+   with(data = df.rs, scatterplot3d(x = df.rs[,1], y = df.rs[,2], z = df.rs[,3]))
+}
