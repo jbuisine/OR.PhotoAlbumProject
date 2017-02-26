4b05059367729bcad2d52221b23d2ad6fb89ad77
Jérôme Buisine <jerome.buisine@outlook.fr>
2017-02-26 22:59:07 +0100

    Update 3D solution representation

--- www/public/template.js
+++ www/public/template.js
@@ -19,13 +19,16 @@ $( "#albumType" ).change(function() {
 function loadSelectSol(solutions) {
     $( "#solutionFile" ).empty();
 
+    console.log(solutions.length);
+
     if(solutions.length > 0){
-        $("#solutionFile").attr();
+        $("#solutionFile").attr('disabled', false);
         $.each(solutions, function( key, sol ) {
             $("#solutionFile").append('<option value="' + sol + '">' + sol + '</option>');
         });
     }
     else{
+        $("#solutionFile").attr('disabled', true);
         $("#solutionFile").append('<option> No solution found</option>');
     }
 }
@@ -48,89 +51,141 @@ function loadContentSol(data) {
     data.splice(0, 1);
 
     switch (head.length){
+
+        case 1:
+            generate2DPlot(data, head);
+            break;
+
         case 2 :
-            var xAxis = [];
-            var yAxis = [];
-
-            data.forEach(function (elem) {
-                xAxis.push(elem[1]);
-                yAxis.push(elem[2]);
-            });
-
-            var line = {
-                x: xAxis,
-                y: yAxis,
-                type: 'scatter',
-                mode: 'markers'
-            };
-
-            var layout = {
-                title: 'Solution file name',
-                xaxis: {
-                    title: head[0],
-                    type: 'log',
-                    autorange: true,
-                    titlefont: {
-                        family: 'Courier New, monospace',
-                        size: 18,
-                        color: '#7f7f7f'
-                    }
-                },
-                yaxis: {
-                    title: head[1],
-                    type: 'log',
-                    autorange: true,
-                    titlefont: {
-                        family: 'Courier New, monospace',
-                        size: 18,
-                        color: '#7f7f7f'
-                    }
-                }
-            };
-
-            Plotly.newPlot('graphic-representation', [line], layout);
+            generate2DPlot(data, head);
             break;
 
         case 3 :
-
-            var xAxis = [];
-            var yAxis = [];
-            var zAxis = [];
-
-            data.forEach(function (elem) {
-                xAxis.push(elem[1]);
-                yAxis.push(elem[2]);
-                zAxis.push(elem[3]);
-            });
-
-            var line1 = {
-                type: 'scatter',
-                mode: 'markers',
-                x: xAxis,
-                y: yAxis,
-                z: zAxis,
-                color: 'blue'
-            };
-
-            var layout = {
-                scene: {
-                    xaxis: {
-                        title: head[0]
-                    },
-                    yaxis: {
-                        title: head[1]
-                    },
-                    zaxis: {
-                        title: head[2]
-                    }
-                },
-                autosize: true
-            };
-
-            Plotly.plot('graphic-representation', [line1], layout);
-
+            generate3DPlot(data, head);
             break;
     }
 
     $('#generation-info-modal').modal();
+}
+
+/**
+ * Utility function used for generate 2D plot
+ *
+ * @param data
+ * @param head
+ */
+function generate2DPlot(data, head){
+
+    var xAxis = [];
+    var yAxis = [];
+
+    data.forEach(function (elem) {
+        xAxis.push(elem[1]);
+        yAxis.push(elem[2]);
+    });
+
+    var line = {
+        x: xAxis,
+        y: yAxis,
+        type: 'scatter',
+        mode: 'markers'
+    };
+
+    var layout = {
+        title: 'Solution file name',
+        xaxis: {
+            title: head[0],
+            type: 'log',
+            autorange: true,
+            titlefont: {
+                family: 'Courier New, monospace',
+                size: 18,
+                color: '#7f7f7f'
+            }
+        },
+        yaxis: {
+            title: head[1],
+            type: 'log',
+            autorange: true,
+            titlefont: {
+                family: 'Courier New, monospace',
+                size: 18,
+                color: '#7f7f7f'
+            }
+        }
+    };
+
+    Plotly.newPlot('graphic-representation', [line], layout);
+}
+
+/**
+ *  Utility function used for generate 3D plot
+ *
+ * @param data
+ * @param head
+ */
+function generate3DPlot(data, head){
+
+    var xAxis = [];
+    var yAxis = [];
+    var zAxis = [];
+
+    data.forEach(function (elem) {
+        xAxis.push(elem[1]);
+        yAxis.push(elem[2]);
+        zAxis.push(elem[3]);
+    });
+
+    var line1 = {
+        type: 'scatter3d',
+        mode: 'markers',
+        x: xAxis,
+        y: yAxis,
+        z: zAxis,
+        marker: {
+            color: 'rgb(23, 190, 207)',
+            size: 2
+        }
+    };
+
+    var layout = {
+        aspectratio: {
+            x: 1,
+            y: 1,
+            z: 1
+        },
+        camera: {
+            center: {
+                x: 0,
+                y: 0,
+                z: 0
+            },
+            eye: {
+                x: 1,
+                y: 1,
+                z: 1
+            },
+            up: {
+                x: 0,
+                y: 0,
+                z: 1
+            }
+        },
+        scene: {
+            xaxis: {
+                title: head[0]
+            },
+            yaxis: {
+                title: head[1]
+            },
+            zaxis: {
+                title: head[2]
+            }
+        },
+        height: 550,
+        autosize: true,
+        title: '3d point clustering'
+    };
+
+    Plotly.newPlot('graphic-representation', [line1], layout);
 }
