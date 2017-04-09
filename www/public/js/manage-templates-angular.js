a4323b63cdfd55327d5a5544cf8175ac8dea9f8d
Jérôme Buisine <jerome.buisine@outlook.fr>
2017-04-09 11:17:25 +0200

    Remove template and display photo of template updated with angular JS

--- www/public/js/manage-templates-angular.js
+++ www/public/js/manage-templates-angular.js
@@ -52,24 +52,45 @@ app.controller('MainController', ['$scope', '$timeout', '$http', '$location', 'M
     manageCtrl.templateName     = "";
     manageCtrl.selectedTemplate = "no";
     manageCtrl.photoTemplate    = [];
+    manageCtrl.photoLoaded      = false;
+
+    manageCtrl.init = function () {
+        //Redirect to home if page is reload
+        if(manageCtrl.selectedTemplate === "no"){
+            $location.path("/");
+        }
+    };
+
+    manageCtrl.init();
 
     manageCtrl.addTemplate = function () {
         manageService.createTemplate(manageCtrl.templateName).then(function () {
-           Location.reload();
+           location.reload();
         });
     };
 
-
-
     manageCtrl.loadPhoto = function () {
+        manageCtrl.photoLoaded = false;
         manageService.displayTemplate(manageCtrl.selectedTemplate).then(function (data) {
             manageCtrl.photoTemplate = data;
+            manageCtrl.photoLoaded = true;
+
             $location.path('/display');
+
             $timeout(function () {
-                $('.gridly').gridly('layout');
+                //Need to set layout two times to have correct display
+                var gridDiv = $('.gridly');
+                gridDiv.gridly('layout');
+                gridDiv.gridly('layout');
             }, 1000);
         });
     };
+
+    manageCtrl.removeTemplate = function () {
+        manageService.removeTemplate(manageCtrl.selectedTemplate).then(function () {
+            location.reload();
+        });
+    }
 }]);
 
 app.controller('HomeController', ['$scope', '$timeout', '$http', 'ManageTemplate', function ($scope, $timeout, $http, manageService, address) {
@@ -87,7 +108,6 @@ app.config(function($routeProvider) {
             templateUrl : "/manage-display/home.html"
         })
         .when("/upload", {
-            controller  : 'UploadController',
             templateUrl : "/manage-display/upload.html"
         })
         .when("/display", {
