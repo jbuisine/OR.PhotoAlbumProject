a70e8f257d1df2f88c78d7aa20b9fac63710f04f
Jérôme Buisine <jerome.buisine@outlook.fr>
2017-06-28 23:54:38 +0200

    Stats R scripted plot criteria comparaison added

--- utilities/stats.R
+++ utilities/stats.R
@@ -38,22 +38,31 @@ main <- function(){
   
   setwd("/home/jbuisine/Documents/M1.I2L/AnnualProject/RO.PhotoAlbumProject/resources/solutions/StatsTemplate/album-4-2per2/")
   
-  filenameStats = "RandomWalk_greyAVG_commonTags_20000.sol.tracking"
-  filename = "RandomWalk_greyAVG_commonTags_20000.sol"
 
   dfRandom <- readFile("RandomWalk_greyAVG_commonTags_20000.sol")
   dfMOEAD_T <- readFile("MOEAD_greyAVG_commonTags_20000_T.sol")
+  dfMOEAD_W <- readFile("MOEAD_greyAVG_commonTags_20000_W.sol")
+  dfPLS <- readFile("PLS_greyAVG_commonTags_20000.sol")
+  dfTPLS_T <- readFile("TPLS_greyAVG_commonTags_20000_10_10_T.sol")
+  dfTPLS_W <- readFile("TPLS_greyAVG_commonTags_20000_10_10_W.sol")
   
   dfStatsRandom <- readFile("RandomWalk_greyAVG_commonTags_20000.sol.tracking")
   dfStatsMOEAD_T <- readFile("MOEAD_greyAVG_commonTags_20000_T.sol.tracking")
   dfStatsMOEAD_W <- readFile("MOEAD_greyAVG_commonTags_20000_W.sol.tracking")
+  dfStatsPLS <- readFile("PLS_greyAVG_commonTags_20000.sol.tracking")
+  dfStatsTPLS_T <- readFile("TPLS_greyAVG_commonTags_20000_10_10_T.sol.tracking")
+  dfStatsTPLS_W <- readFile("TPLS_greyAVG_commonTags_20000_10_10_W.sol.tracking")
   
-  plot(dfMOEAD_T$greyAVG ~ dfMOEAD_T$commonTags,xlab = "Common Tags", ylab = "Grey AVG", data = dfStats, col="blue")
+  # Remove duplicate line iterations
+  dfStatsPLS <- dfStatsPLS[!duplicated(dfStatsPLS[,1]),]
+  dfStatsTPLS_T <- dfStatsTPLS_T[!duplicated(dfStatsTPLS_T[,1]),]
+  dfStatsTPLS_W <- dfStatsTPLS_W[!duplicated(dfStatsTPLS_W[,1]),]
   
-  #plot(dfStats$D ~ dfStats$I, data = dfStats, col="blue")
-  
-  #plot(dfStats$ND ~ dfStats$I, data = dfStats, col="blue")
-  
+  plot(dfTPLS_W$greyAVG ~ dfTPLS_W$commonTags, xlab = "Common Tags", ylab = "", 
+       data = dfTPLS_W, 
+       col="black", pch=1, 
+       ylim=c(2, 10), xlim=c(12,18),
+       main="TPLS W")
   
   # Diferent function
   # colSds(df_bis)
@@ -61,22 +70,39 @@ main <- function(){
   # colMedians(df_bis)
   # colVars(df_bis)
 
-  dfRandom_s <- generateOutpuStats(colMeans, dfStatsRandom[, needed])
-  dfMOEAD_T_s <- generateOutpuStats(colMeans, dfStatsMOEAD_T[, needed])
-  dfMOEAD_W_s <- generateOutpuStats(colMeans, dfStatsMOEAD_W[, needed])
+  dfRandom_s <- generateOutpuStats(colSds, dfStatsRandom[, needed])
+  dfMOEAD_T_s <- generateOutpuStats(colSds, dfStatsMOEAD_T[, needed])
+  dfMOEAD_W_s <- generateOutpuStats(colSds, dfStatsMOEAD_W[, needed])
+  dfPLS_s <- generateOutpuStats(colSds, dfStatsPLS[, needed])
+  dfTPLS_T_s <- generateOutpuStats(colSds, dfStatsTPLS_T[, needed])
+  dfTPLS_W_s <- generateOutpuStats(colSds, dfStatsTPLS_W[, needed])
   
-  plot(HV ~ I ,
+  plot(c(0,20000),
+       c(10,30),
+       type="n",
        xlab = "Iteration", 
-       ylab = "HV", 
-       data = dfRandom_s, 
-       ylim = c(50,150),
-       type = "l",
-       col="black",
-       lty=3)
+       ylab = "",
+       main="Mean")
+       
+  lines(dfRandom_s$ND ~ dfRandom_s$I, col="black", lwd=2, lty=1)
+  lines(dfPLS_s$ND ~ dfPLS_s$I, col="red", lwd=2, lty=2)
+  lines(dfMOEAD_T_s$ND ~ dfMOEAD_T_s$I, col="blue", lwd=2, lty=3)
+  lines(dfMOEAD_W_s$ND ~ dfMOEAD_W_s$I, col="#4d9900", lwd=2, lty=4)
+  lines(dfTPLS_T_s$ND ~ dfTPLS_T_s$I, col="orange", lwd=2, lty=5)
+  lines(dfTPLS_W_s$ND ~ dfTPLS_W_s$I, col="purple", lwd=2, lty=6)
   
-  head(dfRandom_s)
+  legend(14500,
+         29,
+         c("Random walk", "PLS", "MOEAD T", "MOEAD W", "TPLS T","TPLS W"),
+         lty=c(1,2,3,4,5,6),
+         lwd=c(2,2,2,2,2,2),
+         col=c("black","red", "blue","#4d9900","orange", "purple")
+      )
+  
   #lines(dfRandom_s$ND ~ dfRandom_s$I, col = "steelblue")
-  lines(dfMOEAD_T_s$HV ~ dfMOEAD_T_s$I, col = "blue", lty=2)
-  lines(dfMOEAD_W_s$HV ~ dfMOEAD_W_s$I, col = "red", lty=1)
+  #lines(dfMOEAD_T_s$HV ~ dfMOEAD_T_s$I, lty = 2, col="blue")
+  #lines(dfMOEAD_W_s$HV ~ dfMOEAD_W_s$I, lty = 3, col="green")
+  #lines(dfPLS_s$HV ~ dfPLS_s$I, lty = 4, col="red")
+  #lines(dfTPLS_T_s$HV ~ dfTPLS_T_s$I, lty = 5, col="orange")
 }
 
